40e76c9a4a848c7602472c6b98f9d70f8809f3e8
Jessica <jessyvilela12@gmail.com>
2020-08-24 21:54:41 -0300

    exe7 realizado

---
+++ exe7.js
@@ -0,0 +1,24 @@
+    
+    chefe()
+
+    function chefe(){
+        exe7()
+    }
+
+    function exe7(){
+        
+        var soma = 0
+        var conta = 0
+        var num = Number(prompt("Informe um número: "))
+        do{
+            conta ++ // soma 1
+            soma = soma + num // acumulador
+            num = Number(prompt("Informe outro número. /n Digite um número negativo para encerrar"))
+
+            num = Number
+        }
+
+        while(num >= 0)
+
+        console.log(`A média dos números informados é ${soma / conta}`)
+    }
